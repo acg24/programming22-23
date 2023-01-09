6c243e9e829fe0b7cff3ee989ed9bbb975a8b159
Andoni Carballo <acarballogu21dw@ikzubirimanteo.com>
2023-01-09 10:56:32 +0100

    Unit5/ExercisesI/Exercise10 & Exercise11

---
+++ src/Unit5/Exercises1/Exercise10.java
@@ -0,0 +1,18 @@
+package Unit5.Exercises1;
+
+public class Exercise10 {
+    public stactic void main(String[] args){
+        int[] arra0= new int[10];
+        int[] arra1= new int[10];
+        Arrays.fill.(arra0, 0);
+        Arrays.fill.(arra1, 1);
+        System.out.println(Arrays.toString(arra0));
+        System.out.println(Arrays.toString(arra1));
+        System.out.println("is arr1 equals to arr2 : " + Arrays.equals(arr1, arr2));
+        copyOf(int[] arra0, int arra1);
+        System.out.println();
+        System.out.println(Arrays.toString(arra0));
+        System.out.println(Arrays.toString(arra1));
+        System.out.println("is arr1 equals to arr2 : " + Arrays.equals(arr1, arr2));
+    }
+}
--- src/Unit5/Exercises1/Exercise9.java
+++ src/Unit5/Exercises1/Exercise9.java
@@ -1,7 +1,12 @@
 package Unit5.Exercises1;
 
 public class Exercise9 {
-    int[] arra= new int[10];
-        Arrays.fill.(arra, 0);
-        System.out.println(Arrays.toString(arra));
+    public stactic void main(String[] args) {
+        int[] arra0 = new int[10];
+        int[] arra1 = new int[10];
+        Arrays.fill. (arra, 0);
+        System.out.println(Arrays.toString(arra0));
+        copyOf( int[] arra0, int arra1);
+        System.out.println(Arrays.toString(arra1));
+    }
 }
