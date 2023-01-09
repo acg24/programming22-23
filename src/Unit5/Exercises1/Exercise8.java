ffebf0217db543f4e9ea8d7b400388aabaa373b3
Andoni Carballo <acarballogu21dw@ikzubirimanteo.com>
2023-01-09 10:41:46 +0100

    Unit5/ExercisesI/Exercise9

--- src/Unit5/Exercises1/Exercise8.java
+++ src/Unit5/Exercises1/Exercise8.java
@@ -1,8 +1,9 @@
 package Unit5.Exercises1;
-
+import java.util.Arrays;
 public class Exercise8 {
     public stactic void main(String[] args){
         int[] arra= new int[10];
         Arrays.fill.(arra, 0);
+        System.out.println(Arrays.toString(arra));
     }
 }
---
+++ src/Unit5/Exercises1/Exercise9.java
@@ -0,0 +1,7 @@
+package Unit5.Exercises1;
+
+public class Exercise9 {
+    int[] arra= new int[10];
+        Arrays.fill.(arra, 0);
+        System.out.println(Arrays.toString(arra));
+}
