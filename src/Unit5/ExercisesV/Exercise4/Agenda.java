41717706bd31e9146291be4dec18b42f5432aa02
Andoni Carballo <acarballogu21dw@ikzubirimanteo.com>
2023-01-27 11:37:50 +0100

    Unit5/ExercisesV/Exercise4 continue0

--- src/Unit5/ExercisesV/Exercise4/Agenda.java
+++ src/Unit5/ExercisesV/Exercise4/Agenda.java
@@ -1,9 +1,7 @@
 package Unit5.ExercisesV.Exercise4;
 import Unit5.ExercisesV.Exercise3.Movie;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+
+import java.util.*;
 
 public class Agenda {
     private HashMap <String,String[]> entries;
@@ -12,11 +10,75 @@ public class Agenda {
         this.maxPer=mn;
         this.entries = new HashMap<String,String[]>();
     }
+
+    /*
+    =======================================================================================================================================================================
+       -Intro (a name and an array with phone numbers) and add it to the agenda.*/
     public void addEntry(String name, String[] nums){
+        boolean isIn=false;
+        int inPer=0;
+        if (entries.containsKey(name)){
+            isIn=true;
+        }
         Iterator<Map.Entry<String,String[]>> it=entries.entrySet().iterator();
         while (it.hasNext()){
             Map.Entry<String,String[]> map=(Map.Entry<String,String[]>) it.next();
+            inPer++;
             //System.out.println(map.getKey()+ " " + map.getValue());
         }
+        if (isIn ==true){
+            Scanner s0 = new Scanner(System.in);
+            System.out.println();
+            System.out.println("============================================================================");
+            System.out.println("Your agenda contains these phone number for.");
+            System.out.print(name + ":  ");
+            for (int i=0; i<nums.length;i++){
+                System.out.print(nums[i] + ", ");
+            }
+            System.out.println();
+            System.out.println("Want to replace them by new ones?(y/n)");
+            String answer = s0.nextLine();
+            if (answer.equals("y")){
+                entries.replace(name, nums);
+            } else if (answer.equals("n")) {
+                System.out.println("The numbers have not been replaced.");
+            }
+        } else if (isIn==false && inPer<maxPer) {
+            entries.put(name, nums);
+            System.out.println(name + (" is added to the agenda."));
+        }else if(isIn==false && inPer>=maxPer){
+            System.out.println("Full agenda");
+        }
     }
+    public void display(){
+        Iterator<Map.Entry<String,String[]>> it=entries.entrySet().iterator();
+        System.out.println();
+        System.out.println("===================================AGENDA===================================");
+        while (it.hasNext()){
+            Map.Entry<String,String[]> map=(Map.Entry<String,String[]>) it.next();
+            System.out.print(map.getKey()+ ": ");
+            for (int i=0; i<map.getValue().length;i++){
+                System.out.print(map.getValue()[i] + ", ");
+            }
+            System.out.println();
+        }
+    }
+    public void  searchName(String name){
+        System.out.println();
+        System.out.println("============================================================================");
+        boolean isIn=false;
+        if (entries.containsKey(name)){
+            for (int i=0; i<entries.get(name).length;i++){
+                System.out.print(name + ": ");
+                System.out.print(entries.get(name)[i] + ", ");
+            }
+            System.out.println();
+        } else if (!entries.containsKey(name)) {
+            System.out.println(name + " is not in the agenda");
+        }
+    }
+    public void numberOfPhones(int num){
+        
+    }
+
 }
--- src/Unit5/ExercisesV/Exercise4/Test.java
+++ src/Unit5/ExercisesV/Exercise4/Test.java
@@ -2,6 +2,21 @@ package Unit5.ExercisesV.Exercise4;
 
 public class Test {
     public static void main(String[] args) {
-
+        String[] nums0 = new String[]{ "11111","222222","33333","44444","55555"};
+        String[] nums1 = new String[]{ "44444","444444","66666","88888","77777"};
+        String[] nums2 = new String[]{ "77777","888888","66666","77777","11111"};
+        String[] nums3 = new String[]{ "88888","222222","33333","11111","22222"};
+        String[] nums4 = new String[]{ "55555","888888","66666","77777","22222"};
+        Agenda a0 = new Agenda(4);
+        a0.addEntry("jon", nums0);
+        a0.addEntry("mikel", nums1);
+        a0.addEntry("jone", nums2);
+        a0.addEntry("mikele", nums3);
+        a0.display();
+        a0.addEntry("jon", nums4);
+        a0.display();
+        a0.addEntry("iker", nums3);
+        a0.searchName("mikele");
+        a0.searchName("alaine");
     }
 }
