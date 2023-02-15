02d0883fbdae3645de3f498eb98f4e9bb773f39b
Andoni Carballo <acarballogu21dw@ikzubirimanteo.com>
2023-02-15 14:28:26 +0100

    Unit7/ExercisesII/Exercise1/ Exercise (main)doing

--- src/Unit7/ExercisesII/Exercise1/Exercise.java
+++ src/Unit7/ExercisesII/Exercise1/Exercise.java
@@ -1,9 +1,6 @@
 package Unit7.ExercisesII.Exercise1;
 
-import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Exercise {
@@ -12,17 +9,17 @@ public class Exercise {
         System.out.println("Intro a number:");
         int num0= s0.nextInt();
         DataOutputStream dos=null;
-        
+
         try {
             dos =new DataOutputStream(new FileOutputStream("nums.bin", true));
-            try {
-                dos.writeInt((int) num0);
-            } catch (IOException e) {
-                System.out.println("tou didn't intro a number");
-            }
+            FileReader f1=new FileReader(new File("nums.bin"));
+            dos.writeInt(num0);
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }finally {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
             if (dos!=null){
                 try {
                     dos.close();
