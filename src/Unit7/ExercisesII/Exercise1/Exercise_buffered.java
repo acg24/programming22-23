9c3fbf32d96c77afa1b0afe0ae6e4426114cabc0
Andoni Carballo <acarballogu21dw@ikzubirimanteo.com>
2023-02-17 11:35:10 +0100

    Unit7/ExercisesII/Exercise1/ Exercise (main)doing & Exercise_buffered (main)done

--- src/Unit7/ExercisesII/Exercise1/Exercise_buffered.java
+++ src/Unit7/ExercisesII/Exercise1/Exercise_buffered.java
@@ -10,11 +10,11 @@ public class Exercise_buffered {
         Scanner s0 = new Scanner(System.in);
         Scanner s1 = new Scanner(System.in);
         String aux="y";
-        List<Integer> nums=new ArrayList<Integer>();
+        List<String> nums=new ArrayList<String>();
 
         while (aux.equals("y")){
             System.out.println("Intro a number:");
-            int num0= s0.nextInt();
+            String num0= s0.nextLine();
             nums.add(num0);
             System.out.println("do you want to introduce more numbers??(y/n)");
             aux=s1.nextLine();
@@ -23,12 +23,41 @@ public class Exercise_buffered {
         BufferedWriter bw = null;
         try {
             bw = new BufferedWriter(new FileWriter("file1.bin"));
-            bw.write(nums.toString());
-
+            for (int i=0;i<nums.size();i++) {
+                bw.write((nums.get(i)));
+                bw.write(" ");
+            }
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("File not found");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("ERROR writing");
+        }finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        BufferedReader br = null;
+        bw = null;
+        try {
+            br = new BufferedReader(new FileReader("file1.bin"));
+            bw = new BufferedWriter(new FileWriter("file1.text"));
+            
+            String result= br.readLine();
+            String[] array = result.split(" ");
+
+            for (int i=0;i< array.length-1;i++){
+                bw.write(array[i]);
+                bw.newLine();
+            }
+            bw.write(array[array.length-1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("ERROR writing");
         }finally {
             if (bw != null) {
                 try {
